b0be76a60bc887419fe32650c62a72d4d8a34d10
Przemek2940 <89359034+Przemek2940@users.noreply.github.com>
2022-08-29 15:27:06 +0200

    Update app.py
    zmiana a na w

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def index():
 def view_availability():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     query = """select g.id_group, g.name_group, i.id_item, i.name_item, 
-                sum((case when a.item_quant is null then 0 else a.item_quant end)) 
-                from warehouse a
-                    right join items i on a.id_item =  i.id_item
+                sum((case when w.item_quant is null then 0 else w.item_quant end)) 
+                from warehouse w
+                    right join items i on w.id_item =  i.id_item
                     left join item_groups g on i.id_group = g.id_group
                 group by i.id_item, g.id_group
                 order by g.id_group, i.id_item"""
