f3ad6e82c49cf7683a37696b4bc99290552a0536
Przemek2940 <89359034+Przemek2940@users.noreply.github.com>
2022-10-18 16:22:48 +0200

    Update app.py
    Zmiana źródła bazy danych,
    Dodanie klientów,
    Dodanie query2 do order completing

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import psycopg2.extras
 from flask import Flask, render_template, request, redirect, url_for
 
 conn = psycopg2.connect(
-    database="Sprzedaz_magazynowa", user='postgres', password='postgres',
-    host='projectsql3.c4lilzenuyna.eu-central-1.rds.amazonaws.com', port='5432')
+    database="loc_project", user='postgres', password='admin',
+    host='localhost', port='5432')
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/rebuilding')
 def rebuilding():
@@ -59,25 +61,59 @@ def availability_add():
         return render_template('error.html', error=error)
 
 
+@app.route('/view_customers')
+def view_customers():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = """select id_customer, name_customer
+                from customers
+                order by id_customer"""
+    cur.execute(query)
+    cust = cur.fetchall()
+
+    return render_template('customers.html', cust=cust)
+
+
+@app.route('/customers_add', methods=['GET', 'POST'])
+def customers_add():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        name_customer = request.form.get("name_customer")
+
+        cur.execute('INSERT INTO customers(name_customer) VALUES (%s)', (name_customer,))
+        conn.commit()
+        return redirect(url_for('view_customers'))
+    else:
+        error = "Błąd przesyłu danych [54]."
+        return render_template('error.html', error=error)
+
+
 @app.route('/order_completing')
 def order_completing():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    query = """select g.id_group, g.name_group, i.id_item, i.name_item,
+    query1 = """select g.id_group, g.name_group, i.id_item, i.name_item,
                 sum((case when w.item_quant is null then 0 else w.item_quant end)), i.price_item||'zł' 
                 from warehouse w
                     right join items i on w.id_item =  i.id_item
                     left join item_groups g on i.id_group = g.id_group
                 group by i.id_item, g.id_group
                 order by g.id_group, i.id_item"""
-    cur.execute(query)
+    cur.execute(query1)
     oc = cur.fetchall()
-    return render_template('order.html', oc=oc)
+
+    query2 = """select id_customer, name_customer
+                from customers
+                order by id_customer"""
+    cur.execute(query2)
+    cop = cur.fetchall()
+    return render_template('order.html', oc=oc, cop=cop)
+
 
 @app.route('/order_saving')
 def order_saving():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     if request.method == 'POST':
         return 0
+
 
 if __name__ == '__main__':
     app.run(debug=True)
